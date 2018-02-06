0228082429e9368030c362e15af06c1a9f38af59
Diego Fernández Barrera <bigomby@gmail.com>
2018-02-06 13:13:35 +0100

    Bootstrap project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
 
-async function bootstrap() {
-	const app = await NestFactory.create(ApplicationModule);
-	await app.listen(3000);
-}
-bootstrap();
+(async () => {
+  const app = await NestFactory.create(ApplicationModule);
+  await app.listen(3000);
+})();
